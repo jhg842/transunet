b3a27918d03f437398c6fc81ec5f94eeab612e0a
jhg842 <jhg842@naver.com>
2025-03-07 17:14:42 +0900

    revision transformer

---
+++ model/transformer.py
@@ -0,0 +1,21 @@
+import torch
+
+import torch.nn as nn
+
+import torch.nn.functional as F
+
+
+class Transformer(nn.Module):
+    def __init__(self,):
+        super().__init__()
+        
+        self.EncoderLayer = EncoderLayer()
+        
+        self.transformerencoder = TransformerEncoder()
+        
+        
+class TransformerEncoder(nn.Module):
+    def __init__(self,)
+    
+class EncoderLayer(nn.Module):
+    def __init__(self):
