aabad8f60d394f6e1241a6935919f5ac248f2e36
jhg842 <jhg842@naver.com>
2025-03-09 13:57:40 +0900

    add a layers

--- model/transformer.py
+++ model/transformer.py
@@ -6,19 +6,24 @@ import torch.nn.functional as F
 
 
 class Transformer(nn.Module):
-    def __init__(self,):
+    def __init__(self, d_model, nhead, dropout, dim_feedforward, activation):
         super().__init__()
         
-        self.EncoderLayer = EncoderLayer()
+        self.EncoderLayer = EncoderLayer(d_model, nhead, dropout, dim_feedforward, activation)
         
         self.transformerencoder = TransformerEncoder()
         
         
 class TransformerEncoder(nn.Module):
-    def __init__(self,)
-    
-    
+    def __init__(self, encoderlayer, num_layers):
+        self.layers = _get_repeat(encoder_layer, num_layers)
+        self.num_layers = num_layers
+        
     def forward(self):
+        for layer in self.layers:
+            output = layer()
+            
+        return output
     
 class EncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dropout, dim_feedforward, activation):
@@ -30,20 +35,25 @@ class EncoderLayer(nn.Module):
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
         self.activation = _get_activation_fn(activation)
-        self.MLP = nn.Sequential(self.linear1, self.linear2, activation)
+        
         
     def forward(self, x):
         src1 = self.norm1(x)
-        q = k = v = x
+        q = k = v = src1
         src2 = self.attention(q,k,v, attn_mask=None, key_padding_mask = None)[0]
         src2 += x
         
         src3 = self.norm2(src2)
-        src3 = self.MLP(src3)
+        src3 = self.linear1(src3)
+        src3 = self.activation(src3)
+        src3 = self.linear2(src3)
         src3 + src2
         
         return src3
-    
+
+
+def _get_repeat(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
     
 def _get_activation_fn(activation):
     if activation == 'relu':
@@ -54,5 +64,5 @@ def _get_activation_fn(activation):
         return F.glu
     raise RuntimeError(f'activation should be relu/gelu, not{activation}.')
         
-        
+
         
