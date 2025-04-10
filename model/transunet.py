90a1516d225e3ad84001ad8b5c693061f2fdf08d
jhg842 <jhg842@naver.com>
2025-04-10 19:15:54 +0900

    porb -> prob

--- model/transunet.py
+++ model/transunet.py
@@ -33,7 +33,7 @@ class DiceLoss(nn.Module):
         tensor_list = []
         for i in range(self.n_classes):
             temp_prob = input_tensor == i
-            tensor_list.append(temp_porb.unsqueeze(1))
+            tensor_list.append(temp_prob.unsqueeze(1))
             output_tensor = torch.cat(tensor_list, dim=1)
             
         return output_tensor.float()
